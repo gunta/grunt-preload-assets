08c7c1e8d5a841830af25ed579137a0297b25d80
Gunther Brunner <gunta85@gmail>
2013-03-04 18:12:09 +0900

    * Added unit tests

---
+++ test/expected/default_options.js
@@ -0,0 +1,7 @@
+var filesManifest = [
+	{id: 'testFixturesGenericButton', src: 'test/fixtures/genericButton.png', type: createjs.LoadQueue.IMAGE},
+	{id: 'testFixturesGenericButtonOver', src: 'test/fixtures/genericButtonOver.png', type: createjs.LoadQueue.IMAGE},
+	{id: 'testFixturesParallaxHill1', src: 'test/fixtures/parallaxHill1.png', type: createjs.LoadQueue.IMAGE},
+	{id: 'testFixturesSky', src: 'test/fixtures/sky.png', type: createjs.LoadQueue.IMAGE},
+	{id: 'testFixturesImage2', src: 'test/fixtures/image2.jpg', type: createjs.LoadQueue.IMAGE}
+];
--- test/preload_assets_test.js
+++ test/preload_assets_test.js
@@ -30,8 +30,8 @@ exports.preload_assets = {
   default_options: function(test) {
     test.expect(1);
 
-    var actual = grunt.file.read('tmp/default_options');
-    var expected = grunt.file.read('test/expected/default_options');
+    var actual = grunt.file.read('tmp/default_options.js');
+    var expected = grunt.file.read('test/expected/default_options.js');
     test.equal(actual, expected, 'should describe what the default behavior is.');
 
     test.done();
@@ -39,8 +39,8 @@ exports.preload_assets = {
   custom_options: function(test) {
     test.expect(1);
 
-    var actual = grunt.file.read('tmp/custom_options');
-    var expected = grunt.file.read('test/expected/custom_options');
+    var actual = grunt.file.read('tmp/custom_options.js');
+    var expected = grunt.file.read('test/expected/custom_options.js');
     test.equal(actual, expected, 'should describe what the custom option(s) behavior is.');
 
     test.done();
